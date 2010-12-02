5dbe104c302e191343c21be6e0049dba6712f58f
Marco Sampellegrini <m@rcosa.mp>
2010-12-02 15:50:52 +0100

    Forgot the fix on Bitter.

--- lib/bitter.ninja.php
+++ lib/bitter.ninja.php
@@ -15,8 +15,53 @@
 			$this->output .= '</span>';
 			$this->output .= '</span>';
 		}
+
+		protected function processLines() {
+			$tokens = preg_split('%(<span class=".*?">|</span>)%', $this->output, 0, PREG_SPLIT_DELIM_CAPTURE);
+			$stack = array(); $this->output = '';
+			
+			$this->startLine();
+			
+			foreach ($tokens as $token) {
+				// Close:
+				if (preg_match('%^</%', $token)) {
+					array_pop($stack);
+					$this->output .= $token;
+				}
+				
+				// Open:
+				else if (preg_match('%^<%', $token)) {
+					array_push($stack, $token);
+					$this->output .= $token;
+				}
+				
+				else {
+					$characters = preg_split('//', $token);
+					
+					foreach ($characters as $character) {
+						if ($character == "\n") {
+							
+							foreach ($stack as $alt_token) $this->output .= '</span>';
+							$this->endLine();
+						}
+						
+						$this->output .= $character;
+						
+						if ($character == "\n") {
+							$this->startLine();
+							
+							foreach ($stack as $alt_token) $this->output .= $alt_token;
+						}
+					}
+				}
+			}
+			
+			$this->endLine();
+		}
 	}
 	
 /*----------------------------------------------------------------------------*/
-	
+
 	return new BitterFormatNinja();
+
+/*----------------------------------------------------------------------------*/
